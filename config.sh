53d4fcb1eec07c6ad25caf2f4990bb0822c6c0b5
Marcel Ackermann <31695+dreamflasher@users.noreply.github.com>
2019-11-07 17:46:50 +0100

    fastai and pytorch in pip install

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository ppa:graphics-drivers/ppa -y
 
 sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y
 
-sudo apt install unattended-upgrades zsh git-core keychain tmux mosh gcc gparted ubuntu-drivers-common -y
+sudo apt install unattended-upgrades zsh git-core keychain tmux mosh gcc gparted ubuntu-drivers-common python3-pip -y
 sudo snap install micro --classic
 
 sudo ubuntu-drivers autoinstall
@@ -23,6 +23,4 @@ export PATH=/opt/conda/bin:$PATH
 
 conda update conda -y && conda update --all -y
 conda install joblib boost statsmodels wget pyhive psutil plotly natsort pandas tensorflow matplotlib scikit-learn jupyter nb_conda nb_conda_kernels notebook -y
-conda install pytorch torchvision cudatoolkit=10.1 -c pytorch
-pip install jupyter_contrib_nbextensions imtools wandb cachetools tensorboardX opencv-python pyarrow
-conda install -c pytorch -c fastai fastai
+pip install torch torchvision jupyter_contrib_nbextensions imtools wandb cachetools tensorboardX opencv-python pyarrow fastai
