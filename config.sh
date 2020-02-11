ac784a8243982cfb35eb6dc9fbb7afb03344ccd2
Marcel Ackermann <31695+dreamflasher@users.noreply.github.com>
2020-02-11 17:35:27 +0100

    replace anaconda with system python3

--- config.sh
+++ config.sh
@@ -13,17 +13,12 @@ sudo apt install nvidia-cuda-toolkit -y
 
 printf "APT::Periodic::Update-Package-Lists "1";\nAPT::Periodic::Download-Upgradeable-Packages "1";\nAPT::Periodic::AutocleanInterval "7";\nAPT::Periodic::Unattended-Upgrade "7";\n" | sudo tee /etc/apt/apt.conf.d/20auto-upgrades
 
-curl https://repo.anaconda.com/pkgs/misc/gpgkeys/anaconda.asc | gpg --dearmor > conda.gpg && sudo install -o root -g root -m 644 conda.gpg /etc/apt/trusted.gpg.d/ 
-echo "deb [arch=amd64] https://repo.anaconda.com/pkgs/misc/debrepo/conda stable main" | sudo tee /etc/apt/sources.list.d/conda.list
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.8 python3.8-dev python3-pip
 
-sudo apt update -y && sudo apt install conda 
-sudo chmod -R a+rwx /opt/ && sudo chmod -R a+rwx /etc/apt/sources.list.d/
+sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 
-export PATH=/opt/conda/bin:$PATH
-
-conda update conda -y && conda update --all -y
-conda install joblib boost statsmodels wget pyhive psutil plotly natsort pandas tensorflow matplotlib scikit-learn jupyter nb_conda nb_conda_kernels notebook -y
-pip install torch torchvision jupyter_contrib_nbextensions imtools wandb cachetools tensorboardX opencv-python pyarrow fastai
+pip3 install joblib boost statsmodels wget pyhive psutil plotly natsort pandas tensorflow matplotlib scikit-learn jupyter nb_conda nb_conda_kernels notebook torch torchvision jupyter_contrib_nbextensions imtools wandb cachetools tensorboardX opencv-python pyarrow fastai
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
