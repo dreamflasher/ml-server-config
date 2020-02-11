40e067dc57dfd068454907d474127b1002492e1f
Marcel Ackermann <31695+dreamflasher@users.noreply.github.com>
2020-02-11 17:54:55 +0100

    move python installation

--- config.sh
+++ config.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 sudo add-apt-repository ppa:graphics-drivers/ppa -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 
 sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt clean -y && sudo apt autoclean -y
 
-sudo apt install unattended-upgrades zsh git-core keychain tmux mosh gcc gparted ubuntu-drivers-common python3-pip -y
+sudo apt install unattended-upgrades zsh git-core keychain tmux mosh gcc gparted ubuntu-drivers-common python3.8 python3.8-dev python3-pip -y
 sudo snap install micro --classic
 
 sudo ubuntu-drivers autoinstall
@@ -12,9 +13,6 @@ sudo ubuntu-drivers autoinstall
 sudo apt install nvidia-cuda-toolkit -y
 
 printf "APT::Periodic::Update-Package-Lists "1";\nAPT::Periodic::Download-Upgradeable-Packages "1";\nAPT::Periodic::AutocleanInterval "7";\nAPT::Periodic::Unattended-Upgrade "7";\n" | sudo tee /etc/apt/apt.conf.d/20auto-upgrades
-
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt install python3.8 python3.8-dev python3-pip
 
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
 
