cabfb56cf09f7d1e5061f2d37170016765952aa5
Marcel Ackermann <31695+dreamflasher@users.noreply.github.com>
2020-03-04 08:50:28 +0100

    azcopy

--- azure.sh
+++ azure.sh
@@ -1,1 +1,6 @@
 wget -O ~/vsls-reqs https://aka.ms/vsls-linux-prereq-script && chmod +x ~/vsls-reqs && sudo ~/vsls-reqs
+
+wget -O ~/azcopy.tar https://aka.ms/downloadazcopy-v10-linux && tar -xvf azcopy.tar && sudo cp -f ./azcopy_linux_amd64_*/azcopy /usr/bin/
+
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+az login
