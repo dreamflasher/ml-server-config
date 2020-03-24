5026c8445c619a1bf6a6d3300f6293f5209b8cf3
Ubuntu <31695+dreamflasher@users.noreply.github.com>
2020-03-24 14:39:30 +0000

    fix azcopy, format auto shutdown

--- azure/azure-auto-shutdown.py
+++ azure/azure-auto-shutdown.py
@@ -1,17 +1,18 @@
 # curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 # az login
 
-import time
 import os
-import psutil
+import time
 from collections import deque
 
-d = deque(maxlen=120*60)  # only look at the last 120 minutes
+import psutil
+
+d = deque(maxlen=120 * 60)  # only look at the last 120 minutes
 while True:
     cpu = psutil.cpu_percent()
     d.append(cpu)
-    if len(d) >= 120*60:  # wait at least 120 min after reboot
+    if len(d) >= 120 * 60:  # wait at least 120 min after reboot
         if max(d) < 9:  # max smaller than 9 percent auto shutdown
             os.system('az vm deallocate -g "$GROUP" -n "$VMNAME"')
-	    os.system('az vmss deallocate -g "$GROUP" -n "$VMNAME --instance-ids 0"')
+            os.system('az vmss deallocate -g "$GROUP" -n "$VMNAME --instance-ids 0"')
     time.sleep(1)
