cb47d9adc252e759d498b6d2b88274bc76c8df47
Ubuntu <31695+dreamflasher@users.noreply.github.com>
2020-05-05 19:19:03 +0000

    azcopy access

--- azure/azure.sh
+++ azure/azure.sh
@@ -1,6 +1,6 @@
 wget -O ~/vsls-reqs https://aka.ms/vsls-linux-prereq-script && chmod +x ~/vsls-reqs && sudo ~/vsls-reqs
 
-wget -O azcopy.tar https://aka.ms/downloadazcopy-v10-linux && tar -xvf azcopy.tar && sudo cp -f ./azcopy_linux_amd64_*/azcopy /usr/bin/
+wget -O azcopy.tar https://aka.ms/downloadazcopy-v10-linux && tar -xvf azcopy.tar && sudo cp -f ./azcopy_linux_amd64_*/azcopy /usr/bin/ && sudo chmod +777 /usr/bin/azcopy
 
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 az login
