1ec6951d7cf44de8fb7acf113c335d2e6845b6b3
Ubuntu <31695+dreamflasher@users.noreply.github.com>
2020-06-05 08:33:30 +0000

    add data dir

--- azure/azure.sh
+++ azure/azure.sh
@@ -2,5 +2,7 @@ wget -O ~/vsls-reqs https://aka.ms/vsls-linux-prereq-script && chmod +x ~/vsls-r
 
 wget -O azcopy.tar https://aka.ms/downloadazcopy-v10-linux && tar -xvf azcopy.tar && sudo cp -f ./azcopy_linux_amd64_*/azcopy /usr/bin/ && sudo chmod +777 /usr/bin/azcopy
 
+sudo mkdir /data && sudo chmod +777 /data
+
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 az login
