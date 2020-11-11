172230fcb1ffe9654d039b23db35b07f606274bd
Marcel Ackermann <31695+dreamflasher@users.noreply.github.com>
2020-11-11 15:32:13 +0100

    Update azure.sh

--- azure/azure.sh
+++ azure/azure.sh
@@ -24,6 +24,7 @@ WantedBy=multi-user.target"
 
 echo "$shutdownscript" | sudo tee /etc/systemd/system/auto-shutdown.service
 cp azure-auto-shutdown.py ~/
+nano ~/azure-auto-shutdown.py
 sudo systemctl daemon-reload && sudo systemctl enable auto-shutdown && sudo systemctl start auto-shutdown
 
 az login --use-device-code
