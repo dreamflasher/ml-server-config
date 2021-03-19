a1e356f925c62ec2e1c1b7fc95819c2d1b2745d7
marcel <31695+dreamflasher@users.noreply.github.com>
2021-03-19 13:25:25 +0000

    use mailgun to send via https

--- azure/notify-spot-eviction.py
+++ azure/notify-spot-eviction.py
@@ -5,8 +5,19 @@ import urllib.error
 import urllib.parse
 import urllib.request
 
-metadata_url = "http://169.254.169.254/metadata/scheduledevents?api-version=2019-01-01"
+import requests
+
+metadata_url = "http://169.254.169.254/metadata/scheduledevents?api-version=2019-08-01"
 this_host = socket.gethostname()
+
+def send_mail(instance_name, status, eventtype):
+    return requests.post(
+        "https://api.mailgun.net/v3/YOUR_DOMAIN_NAME/messages",
+        auth=("api", "YOUR_API_KEY"),
+        data={"from": "mailgun@YOUR_DOMAIN_NAME>",
+              "to": ["bar@example.com"],
+              "subject": "VM evicted: "+instance_name,
+              "text": f"status: {status}, eventtype: {eventtype}"})
 
 
 def get_scheduled_events():
@@ -25,15 +36,16 @@ def handle_scheduled_events(data):
         resourcetype = evt['ResourceType']
         notbefore = evt['NotBefore'].replace(" ", "_")
         if this_host in resources:
-            print(("+ Scheduled Event. This host " + this_host +
-                   " is scheduled for " + eventtype + " not before " + notbefore))
+            #print(("+ Scheduled Event. This host " + this_host +
+            #       " is scheduled for " + eventtype + " not before " + notbefore))
             # Add logic for handling events here
+            send_mail(this_host, status, eventtype)
 
 
 def main():
     while True:
         handle_scheduled_events(get_scheduled_events())
-        time.sleep(10)
+        time.sleep(5)
 
 
 if __name__ == '__main__':
