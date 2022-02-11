aa81c8b69ee92e54b9d7c3b2802ab7a4ee20c56a
Marcel Ackermann <31695+dreamflasher@users.noreply.github.com>
2022-02-11 18:24:44 +0100

    fix docker setup

--- config.sh
+++ config.sh
@@ -92,6 +92,8 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docke
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo gpasswd -a $USER docker
 
 # tmux
 
