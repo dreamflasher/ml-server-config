d4083c1274c11e9cef2bd91ad72ca840d163e050
Marcel Ackermann <31695+dreamflasher@users.noreply.github.com>
2022-03-06 18:28:24 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -95,6 +95,11 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
 sudo gpasswd -a $USER docker
 
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list \
+   && sudo apt-get update && sudo apt-get install -y nvidia-docker2 && sudo systemctl restart docker
+
 # tmux
 
 tmux
