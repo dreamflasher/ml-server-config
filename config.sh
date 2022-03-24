8234653b2ee0809f6c4aed1d4b9cc12bae94940f
Marcel Ackermann <31695+dreamflasher@users.noreply.github.com>
2022-03-24 18:18:07 +0100

    docker-compose

--- config.sh
+++ config.sh
@@ -92,7 +92,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docke
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
 sudo groupadd docker
 sudo gpasswd -a $USER docker
 
