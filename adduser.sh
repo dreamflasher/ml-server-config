54fc1d2c782b14174f3a6dc535a74b6028f6c6d5
Marcel Ackermann <31695+dreamflasher@users.noreply.github.com>
2022-06-03 18:01:01 +0200

    create empty /home/$1/.ssh/authorized_keys

--- adduser.sh
+++ adduser.sh
@@ -6,6 +6,7 @@ sudo useradd -s /bin/bash -d /home/$1/ -m -G sudo $1
 sudo usermod -aG sudo $1
 sudo mkdir /home/$1/.ssh/
 sudo chmod 0700 /home/$1/.ssh/
+sudo touch /home/$1/.ssh/authorized_keys
 sudo grep -xqF -- "$2" /home/$1/.ssh/authorized_keys || sudo -- sh -c "echo '$2' > /home/$1/.ssh/authorized_keys"
 sudo chown -R $1:$1 /home/$1/.ssh/
 
